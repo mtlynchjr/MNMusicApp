9ebbf57b1903cf8bd4d8586209b4854777fd77e9
“jy8474np” <jy8474np@go.minnstate.edu>
2020-11-24 11:34:31 -0600

    Corrected directory levels for models and forms imports in views_photo.py

--- lmn/views/views_photos.py
+++ lmn/views/views_photos.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
-from .models import Photo
-from .forms import PhotosForm
+from ..models import Photo
+from ..forms import PhotosForm
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
