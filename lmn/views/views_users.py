60fcfa255bcf023e22a36e02b756ca77f78bbdf1
“jy8474np” <jy8474np@go.minnstate.edu>
2020-11-29 19:33:16 -0600

    Photo andor PhotosForm added to imports.

--- lmn/views/views_users.py
+++ lmn/views/views_users.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
-from ..models import Venue, Artist, Note, Show
-from ..forms import VenueSearchForm, NewNoteForm, ArtistSearchForm, UserRegistrationForm
+from ..models import Venue, Artist, Note, Show, Photo
+from ..forms import VenueSearchForm, NewNoteForm, ArtistSearchForm, UserRegistrationForm, PhotosForm
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
