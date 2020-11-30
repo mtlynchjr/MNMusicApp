446864d6f4381f54352c53183d4823c6b5e58704
“jy8474np” <jy8474np@go.minnstate.edu>
2020-11-29 19:28:39 -0600

    urls.py updated and PhotosForm added to import

--- lmn/views/views_artists.py
+++ lmn/views/views_artists.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
 from ..models import Venue, Artist, Note, Show
-from ..forms import VenueSearchForm, NewNoteForm, ArtistSearchForm, UserRegistrationForm
+from ..forms import VenueSearchForm, NewNoteForm, ArtistSearchForm, UserRegistrationForm, PhotosForm
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
